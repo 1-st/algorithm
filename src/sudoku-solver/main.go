4c2a8c2c01d1d0f8bbd87278aa534ef68b99c621
UsrAlpha <twx19975261083@gmail.com>
2020-09-20 18:19:51 +0800

    sudoku-solver bit operation version

--- src/sudoku-solver/main.go
+++ src/sudoku-solver/main.go
@@ -11,13 +11,15 @@ package main
 }
 */
 
+/*
+	尝试用位运算优化空间效率
+*/
+
 type Solution struct {
-	board  *[][]byte
-	Empty  [][2]int
-	Row    [9][9]bool
-	Column [9][9]bool
-	Box    [9][9]bool
-	Stop bool
+	board *[][]byte
+	Empty []int  //                     000000000(i)         000000000(j)
+	State [9]int // 00000 000000000(row) 000000000(column) 000000000(box)
+	Stop  bool
 }
 
 func (s *Solution) bfs(solved int) {
@@ -25,8 +27,8 @@ func (s *Solution) bfs(solved int) {
 		s.Stop = true
 		return
 	}
-	i, j := s.Empty[solved][0], s.Empty[solved][1]
-	for k := 0; k < 9&&!s.Stop; k++ {
+	i, j := s.Empty[solved]>>9, s.Empty[solved]&0x1ff
+	for k := 0; k < 9 && !s.Stop; k++ {
 		if s.canSet(i, j, k) {
 			s.setState(i, j, k, true)
 			(*s.board)[i][j] = byte(k + '1')
@@ -37,28 +39,37 @@ func (s *Solution) bfs(solved int) {
 }
 
 func (s *Solution) canSet(i, j, n int) bool {
-	if !s.Row[i][n] && !s.Column[j][n] && !s.Box[(i/3)*3+j/3][n] {
+	if s.State[i]&(1<<n) == 0 && s.State[j]&(1<<(n+9)) == 0 && s.State[(i/3)*3+j/3]&(1<<(n+18)) == 0 {
 		return true
 	}
 	return false
 }
 
 func (s *Solution) setState(i, j, n int, ok bool) {
-	s.Row[i][n] = ok
-	s.Column[j][n] = ok
-	s.Box[(i/3)*3+j/3][n] = ok
+	if ok {
+		s.State[i] |= 1 << n
+		s.State[j] |= 1 << (n + 9)
+		s.State[(i/3)*3+j/3] |= 1 << (n + 18)
+	} else {
+		s.State[i] &^= 1 << n
+		s.State[j] &^= 1 << (n + 9)
+		s.State[(i/3)*3+j/3] &^= 1 << (n + 18)
+	}
 }
 
 func solveSudoku(board [][]byte) {
 	var s = Solution{
 		board: &board,
-		Empty: make([][2]int, 0),
+		Empty: make([]int, 0),
 	}
 
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board); j++ {
 			if board[i][j] == '.' {
-				s.Empty = append(s.Empty, [2]int{i, j})
+				var m = 0
+				m |= j
+				m |= i<<9
+				s.Empty = append(s.Empty, m)
 			} else {
 				n := int(board[i][j] - '1')
 				s.setState(i, j, n, true)
