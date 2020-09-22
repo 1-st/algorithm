d18d2f1ae60a81f193e0e0bfda8f57b3e05a3e65
UsrAlpha <twx19975261083@gmail.com>
2020-09-22 21:02:42 +0800

    first-missing-positive

---
+++ src/first-missing-positive/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestFirstMissingPositive(t *testing.T) {
+	var cases = [][]int{
+		{5, 0, 2, 2, 4, 0, 1, 0, 1, 3},
+		{3, -1, 4, 2, 1, 9, 10},
+		{2, 1},
+		{
+			3, 1, 2, 0,
+		},
+		{
+			2, 3, 4, -1, 1,
+		},
+		{
+			1, 7, 8, 9, 11, 12,
+		},
+	}
+	for k, v := range cases {
+		if firstMissingPositive(v[1:]) != v[0] {
+			t.Error("unmatched:", k)
+		}
+	}
+}
