a025b6b7c12f5a33f81a4ff040887ea2be81397f
UsrAlpha <twx19975261083@gmail.com>
2020-09-24 09:42:08 +0800

    multiply-strings

---
+++ src/multiply-strings/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestMultiply(t *testing.T) {
+	var cases = [][3]string{
+		{"123", "456", "56088"},
+		{"2", "3", "6"},
+		{"0", "0", "0"},
+	}
+	for k, v := range cases {
+		if multiply(v[0], v[1]) != v[2] {
+			t.Error("unmatched:", k)
+		}
+	}
+}
