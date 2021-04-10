d1f4cff9ee0b014e05629091fb3bdc3c9da90b15
UsrAlpha <twx19975261083@gmail.com>
2021-04-10 13:31:28 +0800

    offer.06

---
+++ src/offer.06.cong-wei-dao-tou-da-yin-lian-biao-lcof/cpp/main.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <list>
+
+/*
+{
+  from:"https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/" ,
+  description:"从尾到头打印链表"
+}
+*/
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {
+
+  }
+};
+class Solution {
+ public:
+  vector<int> reversePrint(ListNode *head) {
+    list<int> stack;
+    while (head != NULL) {
+      stack.push_back(head->val);
+      head = head->next;
+    }
+    vector<int> ret;
+    while (!stack.empty()) {
+      ret.push_back(stack.back());
+      stack.pop_back();
+    }
+    return ret;
+  }
+};
+
+int main() {
+  ListNode n1(1), n2(3), n3(2);
+  n1.next = &n2;
+  n2.next = &n3;
+  Solution s;
+  s.reversePrint(&n1);
+  return 0;
+}
